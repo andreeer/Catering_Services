a6d244b2df69a58c83f5e17813ee13f097820325
DevJ25 <alavrezsanti@gmail.com>
2025-04-16 21:06:46 -0500

    Creacion de los archivos frotend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Hola aca haremos lo del frotend</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
